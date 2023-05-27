796cb11a8f53494934dd3b0fd663f3a5e5e33d59
Ievgennii <simonovievgenii@gmail.com>
2023-05-27 21:33:09 +0300

    change styles

--- src/Pages/Home/Home.styled.js
+++ src/Pages/Home/Home.styled.js
@@ -10,20 +10,6 @@ export const MovieLink = styled(Link)`
     box-shadow: 1px 2px 4px 3px rgba(0, 0, 0, 0.5);
   }
 `;
-export const MovieList = styled.ul`
-display: grid;
-  max-width: calc(100vw - 48px);
-  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
-  grid-template-columns: repeat(auto-fill, minmax(320px, 1fr));
-  grid-gap: 12px;
-  margin-top: 0;
-  margin-bottom: 0;
-  padding: 0;
-  list-style: none;
-  margin-left: auto;
-  margin-right: auto;
-
-`;
 
 export const Title = styled.h2`
   
--- src/components/Cast/Cast.styled.js
+++ src/components/Cast/Cast.styled.js
@@ -1,7 +1,11 @@
 import styled from 'styled-components';
 
 export const CastList = styled.ul`
-
+display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+  gap: 30px;
+  padding: 30px 0;
   list-style: none;
 `;
 export const CastItem = styled.li`
